e067a296f4fbfd0d25d35bbd8f6f446a57d9719b
BastiRenken <sebastian-renken@t-online.de>
2017-08-30 09:37:41 +0200

    Add JavaScript

---
+++ script1.js
@@ -0,0 +1,4 @@
+alert("Diese Webseite verwendet JavaScript. Durch die Benutzung dieser Webseite erklären sie sich damit einverstanden.");
+
+var textinhalt = document.getElementById("textinhalt");
+console.log(textinhalt.innerHTML);
