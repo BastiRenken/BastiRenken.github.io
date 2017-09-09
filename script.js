b76a1478d86c700eaf1b1283d0f453cf31594b83
BastiRenken <sebastian-renken@t-online.de>
2017-09-09 10:57:14 +0200

    Update

--- script.js
+++ script.js
@@ -4,21 +4,39 @@ document.getElementById("ueberschrift").onmouseover = function() {
 document.getElementById("ueberschrift").onmouseout = function() {
   mouseOut()
 };
-
 function mouseOver() {
   document.getElementById("ueberschrift").style.color = "chartreuse";
 }
-
 function mouseOut() {
   document.getElementById("ueberschrift").style.color = "midnightblue";
 }
 
+document.getElementById("orangebutton").onclick = function() {
+  orangebutton()
+}
+function orangebutton() {
+  document.getElementById("body").style.backgroundColor = "orange";
+}
+document.getElementById("lavenderbutton").onclick = function() {
+  lavenderbutton()
+}
+function lavenderbutton() {
+  document.getElementById("body").style.backgroundColor = "lavender";
+}
+document.getElementById("greenbutton").onclick = function() {
+  greenbutton()
+}
+function greenbutton() {
+  document.getElementById("body").style.backgroundColor = "chartreuse";
+}
+/*
 function showTime() {
   var jetzt = new Date();
   var time = jetzt.toLocaleDateString() + ", " + jetzt.toLocaleTimeString();
   document.getElementById("uhr").textContent = time;
 }
 window.setInterval(showTime, 1000);
+*/
 
 /*
 document.getElementsByClassName("inhaltlinks")[0].onmouseover = function() {mouseOver()};
